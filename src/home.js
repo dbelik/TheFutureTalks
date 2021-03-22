3d917bab2dc514394955b7184c788905e2b1c486
Denis Belik <denis.belik2014@gmail.com>
2021-03-22 18:42:59 +0200

    make home page html structure

--- src/home.js
+++ src/home.js
@@ -1,1 +1,11 @@
-import "styles/config/config.scss"
+// Load home style
+import '../styles/pages/home.scss';
+
+// Load font awesome (lazy load)
+import('@fortawesome/fontawesome-free/js/fontawesome')
+import('@fortawesome/fontawesome-free/js/solid')
+import('@fortawesome/fontawesome-free/js/regular')
+import('@fortawesome/fontawesome-free/js/brands')
+
+// Load bootstrap
+import 'bootstrap'
